309e6bd1b63e636e3b7cc889e47e9ab454ccfbfb
Virgil Sweet <virgilsweet@Virgils-MacBook-Air.local>
2026-06-18 12:53:53 -0400

    Update onboarding form with client config system

---
+++ client-config.js
@@ -0,0 +1,13 @@
+const CLIENTS = {
+  'melvin-lara': {
+    name: 'Melvin Lara',
+    greeting: 'Melvin',
+    businesses: 'Real Estate Brokerage, Transportation, Tax',
+    vertical: 'Multi-business Command',
+    placeholder_businesses: 'Lara Real Estate Group\nLara Luxury Transportation\nLara Tax Services',
+    formId: 'mjgdqraa'
+  }
+  // New clients get added here
+};
+
+if (typeof module !== 'undefined') module.exports = CLIENTS;
